a5298a2d5f91a4060b6b675210e38f0996349254
Aruna Turay <tura_arun@yahoo.com>
2024-02-05 14:09:55 -0500

    adds the schema model into the models colder and exporting it to the routes files

---
+++ models/Idea.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');//we bring in mongoose
+
+const IdeaSchema = new mongoose.Schema({ //create a new mongoose schema instance. It takes an object with all the properties that a schema should include
+    text: {
+        type: String,
+        required: [true, 'Please add a text field']//giving us backend validation
+    },
+    tag: {
+        type: String,   
+    },
+    username: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Idea', IdeaSchema); //exporting the Idea file and the Idea schema we just created
