6125bde69a82bce5d0f8dd4f128d09c17c85478b
Aruna Turay <tura_arun@yahoo.com>
2024-02-05 17:40:06 -0500

    adds event listeners to the modal and modal botton

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,11 @@
-import './css/style.css';
 import '@fortawesome/fontawesome-free/css/all.css'; //brings in fontawesome here
+import './css/style.css';
 
+const modal = document.querySelector('#modal');
+const modalBtn = document.querySelector('#modal-btn');
+
+function open() {
+    modal.style.display = 'block';
+}
+
+modalBtn.addEventListener('click', open);
