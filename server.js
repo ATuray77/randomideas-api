b569e1b851b72f35a50d31d17ade0835814a23b9
Aruna Turay <tura_arun@yahoo.com>
2024-02-06 12:41:02 -0500

    configuring cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');//bring this in in order to use path.join method
 const express = require('express'); //access express
+const cors = require('cors');//enables accessing backend resources from the frontend
 require('dotenv').config();//accesses .env file
 const port = process.env.PORT || 5000; //access db thorough either the env port or port 5000
 const connectDB = require('./config/db')// access connections variable
@@ -14,6 +15,12 @@ app.use(express.static(path.join(__dirname, 'public')));//a middleware to make t
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//cors middleware. provides an array of url enable to make request from
+//app.use(cors())//this would allow request from anywhere. 
+app.use(cors({ //this allows to designate ports from which to make requests from
+  origin: ['http://localhost:5000, http://localhost:3000']
+}))
 
 app.get('/', (req, res) => {
     res.json({ Message: 'Welcome to the RandomIdeas API'});
